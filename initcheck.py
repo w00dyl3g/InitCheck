07320b1d482db37e4672c5b2de233d1dec56543a
giacomosighinolfi <giacomosighinolfi@gmail.com>
2022-04-04 18:29:15 +0200

    added code

--- initcheck.py
+++ initcheck.py
@@ -22,18 +22,36 @@ def check_ssh(hostname, username, password):
             return False
     else:
         try:
-            _ssh.connect(hostname=hostname, port=port, username=username, password=password)
+            _ssh.connect(hostname=hostname, username=username, password=password)
             return True
         except:        
             return False        
 
-
+"""
 def ssh():
+    print("[!] Starting Ssh-ing..")
     global output
     output['ssh'] = {}
     for host in config['hosts']:
-        for username,password in config['credentials'].items():
+        #for username,password in config['credentials'].items():
+        for cred in config['credentials']:
+            username = cred[0] #new with tuple
+            password = cred[1] #new with tuple
             output['ssh'][host+" ("+username+":"+password+")"] = check_ssh(host,username,password)
+    print("[+] Finished Ssh-ing!")
+"""
+def ssh():
+    print("[!] Starting Ssh-ing..")
+    global output
+    output['ssh'] = {}
+    for cred in config['credentials']:
+        username = cred[0] #new with tuple
+        password = cred[1] #new with tuple
+        output['ssh']["("+username+":"+password+")"] = dict()
+        for host in config['hosts']:
+            output['ssh']["("+username+":"+password+")"][host] = check_ssh(host,username,password)
+    print("[+] Finished Ssh-ing!")
+
 
 def check_ping(hostname):
     hostname = hostname.split(":")[0]
@@ -44,10 +62,12 @@ def check_ping(hostname):
         return False
 
 def ping():
+    print("[!] Starting Pinging..")
     global output
     output['ping'] = {}
     for host in config['hosts']:
         output['ping'][host.split(":")[0]] = check_ping(host)
+    print("[+] Finished Pinging!")
 
 def do_checks():
     for check in config['checks']:
@@ -61,11 +81,45 @@ def parse_yaml(filename):
         except yaml.YAMLError as exc:
             print(exc)
 
+def print2screen():
+    global output
+    print("[!] Printing ping..")
+    for host in output['ping']:
+        print(output['ping'][host])
+    print("[!] Printing ssh..")
+    for cred in output['ssh']:
+        print(cred)
+        for host in output['ssh'][cred]:
+            print(output['ssh'][cred][host])
+
 def main():
+    print("[!] Loading Config YAML..")
     parse_yaml("config.yaml")
-    pprint.pprint(config)
+    print("[+] Loaded Config YAML!")
+    
+    #debug
+    #pprint.pprint(config)
+    
+    print("[!] Starting Checks..")
     do_checks()
-    pprint.pprint(output)
+    
+    #debug
+    #pprint.pprint(output)
+    
+    print("[+] Ended Checks!")
+    print("[BETA] Printing to Screen..")
+    print2screen()
+    print("[BETA] Printed to Screen!")
+    
+    print("[END] Copy it to excel file!")
+
+    #todo...
+
+    #print("[!] Generating output.csv..")
+    #export2csv()
+    #print("[+] Generated output.csv!")
+
+
 
 if __name__ == "__main__":
     main()
