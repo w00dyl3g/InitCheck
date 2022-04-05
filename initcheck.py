4679e6b08c45e7cd35e9f0c15b3e9c761878ca11
giacomosighinolfi <giacomosighinolfi@gmail.com>
2022-04-05 09:52:53 +0200

    edit code

--- initcheck.py
+++ initcheck.py
@@ -1,7 +1,9 @@
-import os, yaml, pprint, paramiko
+import os, yaml, pprint, paramiko, sys, datetime, xlsxwriter
+
 #support old or unsafe ciphers
 paramiko.Transport._preferred_kex = ('diffie-hellman-group-exchange-sha256','diffie-hellman-group14-sha256','diffie-hellman-group-exchange-sha1',
 'diffie-hellman-group14-sha1','diffie-hellman-group1-sha1')
+
 #init ssh
 _ssh = paramiko.SSHClient()
 _ssh.load_system_host_keys()
@@ -11,35 +13,23 @@ _ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 config = {}
 output = {}
 
+
 def check_ssh(hostname, username, password):
     if ":" in hostname:
         hostname,port = hostname.split(":")[0],hostname.split(":")[1]
         #print(hostname, port)
         try:
             _ssh.connect(hostname=hostname, port=port, username=username, password=password,timeout=10)
-            return True
+            return "OK"
         except:        
-            return False
+            return "NO"
     else:
         try:
             _ssh.connect(hostname=hostname, username=username, password=password, timeout=10)
-            return True
+            return "OK"
         except:        
-            return False        
+            return "NO"        
 
-"""
-def ssh():
-    print("[!] Starting Ssh-ing..")
-    global output
-    output['ssh'] = {}
-    for host in config['hosts']:
-        #for username,password in config['credentials'].items():
-        for cred in config['credentials']:
-            username = cred[0] #new with tuple
-            password = cred[1] #new with tuple
-            output['ssh'][host+" ("+username+":"+password+")"] = check_ssh(host,username,password)
-    print("[+] Finished Ssh-ing!")
-"""
 def ssh():
     print("[!] Starting Ssh-ing..")
     global output
@@ -57,9 +47,9 @@ def check_ping(hostname):
     hostname = hostname.split(":")[0]
     response = os.system("ping -c 1 " + hostname + " > /dev/null 2>&1")
     if response == 0:
-        return True
+        return "OK"
     else:
-        return False
+        return "NO"
 
 def ping():
     print("[!] Starting Pinging..")
@@ -81,43 +71,58 @@ def parse_yaml(filename):
         except yaml.YAMLError as exc:
             print(exc)
 
-def print2screen():
-    global output
-    print("[!] Printing ping..")
+def export2xlsx():
+    filename = sys.argv[1]
+    sheetname = datetime.datetime.now().strftime("%d-%m-%Y")
+
+    #init xlsx
+    workbook = xlsxwriter.Workbook(filename)
+    worksheet = workbook.add_worksheet(sheetname)
+    
+    row = 1
+    col = 0
+    worksheet.write(0, 0, "Host")
+    worksheet.write(0, 1, "Result")
+
+    #ping
     for host in output['ping']:
-        print(output['ping'][host])
-    print("[!] Printing ssh..")
+        worksheet.write(row, 0, host)
+        worksheet.write(row, 1, output['ping'][host])
+        row += 1
+    
+    #ssh
+    col += 2 #blankspaces
     for cred in output['ssh']:
-        print(cred)
+        worksheet.write(0, col, cred)
+        row = 1  
         for host in output['ssh'][cred]:
-            print(output['ssh'][cred][host])
+            worksheet.write(row, col, output['ssh'][cred][host])
+            row += 1
+        col += 1
+    workbook.close()   
+
+def safe_checks():
+    if len(sys.argv) != 2:
+        print("[E] Missing arguments!")
+        print("[E] Leaving..")
+        sys.exit(1)
 
 def main():
+
+    print("[!] Checking arguments..")
+    safe_checks()
+    print("[!] Checking arguments..")
+
     print("[!] Loading Config YAML..")
     parse_yaml("config.yaml")
     print("[+] Loaded Config YAML!")
     
-    #debug
-    #pprint.pprint(config)
-    
     print("[!] Starting Checks..")
     do_checks()
     
-    #debug
-    #pprint.pprint(output)
-    
-    print("[+] Ended Checks!")
-    print("[BETA] Printing to Screen..")
-    print2screen()
-    print("[BETA] Printed to Screen!")
-    
-    print("[END] Copy it to excel file!")
-
-    #todo...
-
-    #print("[!] Generating output.csv..")
-    #export2csv()
-    #print("[+] Generated output.csv!")
+    print("[!] Generating output.xlsx..")
+    export2xlsx()
+    print("[+] Generated output.xlsx!")
 
 
 
